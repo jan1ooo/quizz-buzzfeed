65305b0e3d2f1e6d64ebb6fb583a3746e76143cc
Jânio Filho <filhojanio67@gmail.com>
2023-05-26 22:53:04 -0300

    Resultados dinâmicos

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -36,18 +36,36 @@ export class QuizzComponent implements OnInit {
 
   playerChoose(value:string){
     this.answers.push(value);
+    this.nextStep();
     console.log(this.answers)
   }
 
   async nextStep(){
-
     this.questionIndex++;
 
     if(this.questionIndex < this.questionMaxIndex){
       this.questionSelected = this.questions[this.questionIndex];
     }else{
+      const finalAnswer:string = await this.checkResult(this.answers);
       this.finished = true;
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results];
+      console.log(this.answers)
     }
   }
 
+  async checkResult(answers:string[]){
+    const result = answers.reduce((previous, current, i, arr) => {
+
+      if(arr.filter(item => item === previous).length > arr.filter(item => item === current).length){
+        return previous;
+      }else{
+        return current
+      }
+
+    })
+
+    return result;
+  }
 }
+
+
